9dbf525b55158f97c5acd46549bc69e21ef53006
Ivan-tv <tulya92tv@gmail.com>
2020-01-28 17:58:42 +0300

    Added ui tests, page-object files, product data.

---
+++ cypress/page-object/accessoriesPage.js
@@ -0,0 +1,20 @@
+class AccessoriesPage {
+    open() {
+        cy.visit(`${Cypress.env('googleUrl')}/collection/accessories_wall?hl=en-US`);
+    }
+
+    get searchIcon() {
+        return cy.get('.header-search-icon');
+    }
+
+    get searchInput() {
+        return cy.get('input[aria-label ="Search Google Store"]');
+    }
+
+    performSearch(productToSearch) {
+        this.searchIcon.click();
+        this.searchInput.type(`${productToSearch}{enter}`);
+    }
+}
+
+export default new AccessoriesPage()
